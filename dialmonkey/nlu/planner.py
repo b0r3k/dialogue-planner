4739e1ebe79b832a3fca8fb60df65b624e368ba2
Borek Požár <pozar.borek@gmail.com>
2021-04-09 13:37:50 +0200

    Changed from naive substring search to regex.

--- dialmonkey/nlu/planner.py
+++ dialmonkey/nlu/planner.py
@@ -1,7 +1,7 @@
 from ..component import Component
 from ..da import DAI
 import datetime
-
+import re
 
 class PlannerNLU(Component):
     """A planner NLU that is able to parse commands for manipulating events in calendar and planning commute."""
@@ -9,29 +9,28 @@ class PlannerNLU(Component):
     def __call__(self, dial, logger):
 
         # Detection of confirm intent
-        if any([w in dial.user for w in ["ano", "jo"]]):
+        if re.search(r"\b(an|j)o+\b", dial.user):
             dial.nlu.append(DAI(intent="confirm", slot="value", value="true"))
-        if "ne" in dial.user:
+        if re.search(r"\bne+\b", dial.user):
             dial.nlu.append(DAI(intent="confirm", slot="value", value="false"))
 
         # Detection of task intent
-        if any([w in dial.user for w in ["přidávám", "přidat"]]):
+        if re.search(r"\bpřid[aáe]\S*\b", dial.user):
             dial.nlu.append(DAI(intent="task", slot="goal", value="add_event"))
-        
-
-        # Detection of task intent
+        if re.search(r"\bs?ma[zž]\S*\b", dial.user) or re.search(r"\bodeb[er]\S*\b", dial.user):
+            dial.nlu.append(DAI(intent="task", slot="goal", value="delete_event"))
 
         # Detection of inform intent - slot date
-        if "zítra" in dial.user:
+        if re.search(r"\bzít[rř]\S*\b", dial.user):
             value = datetime.date.today() + datetime.timedelta(days=1)
             dial.nlu.append(DAI(intent="inform", slot="date", value=str(value)))
-        if "pozítří" in dial.user:
+        if re.search(r"\bpozít[rř]\S*\b", dial.user):
             value = datetime.date.today() + datetime.timedelta(days=2)
             dial.nlu.append(DAI(intent="inform", slot="date", value=str(value)))
-        if "včera" in dial.user:
+        if re.search(r"\bvčer\S*\b", dial.user):
             value = datetime.date.today() + datetime.timedelta(days=-1)
             dial.nlu.append(DAI(intent="inform", slot="date", value=str(value)))
-        if "předevčírem" in dial.user:
+        if re.search(r"\bpřede?vč(e|í)r\S*\b", dial.user):
             value = datetime.date.today() + datetime.timedelta(days=-2)
             dial.nlu.append(DAI(intent="inform", slot="date", value=str(value)))
         # TODO parsing of date in numbers / names of day or months
