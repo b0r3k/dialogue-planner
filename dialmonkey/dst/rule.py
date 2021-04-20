6c5b7c0f6ba4ead16119e2896c02032c684d97ba
Borek Požár <pozar.borek@gmail.com>
2021-04-20 19:21:23 +0200

    Dialog state tracker working for the statistical NLU.

--- dialmonkey/dst/rule.py
+++ dialmonkey/dst/rule.py
@@ -1,16 +1,31 @@
 from ..component import Component
 
 
-class DummyDST(Component):
-    """A basic state tracker that simply copies NLU results into the current dialogue
-    state, overwriting any previous values."""
+class BeliefStateDST(Component):
+    """Belief state tracker which copies NLU results into the current dialogue
+    state, multiplies probabilities by None and then adds all other probabilities."""
 
     def __call__(self, dial, logger):
         if dial.nlu:
-            dial.state.intent = dial.nlu[0].intent   # assume just one intent
-            for dai in dial.nlu:
-                if dai.slot:
-                    dial.state[dai.slot] = dai.value
+            dial.state.intent = []
+            for value_probs in dial.nlu:
+                info = value_probs.pop("info_")
+                intent, slot = info["intent"], info["slot"]
+                
+                if not (intent, slot) in dial.state:
+                    dial.state[(intent, slot)] = {None: 1.0}
+                
+                print(dial.state)
+                none_prob = float(value_probs.pop(None))
+                for key in dial.state[(intent, slot)]:
+                    dial.state[(intent, slot)][key] *= none_prob
+
+                for key in value_probs:
+                    if key in dial.state[(intent, slot)]:
+                        dial.state[(intent, slot)][key] += value_probs[key]
+                    else:
+                        dial.state[(intent, slot)][key] = value_probs[key]
+
         else:
             dial.state.intent = None
         logger.info('State: %s', str(dial.state))
